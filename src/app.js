e88d5fcd280bc1d1f15a5a4f2a07f3740b9f131a
hwu71 <hwu71@binghamton.edu>
2019-04-14 15:23:43 -0400

    Add some function

--- src/app.js
+++ src/app.js
@@ -1,8 +1,8 @@
 
 //// node
 
-//var Web3 = require('web3')
-//console.log(Web3)
+var Web3 = require('web3')
+console.log(Web3)
 
 //var EthCrypto = require('eth-crypto')
 //console.log(EthCrypto)
@@ -12,14 +12,13 @@
 //console.log(EthCrypto);
 //const identity = EthCrypto.createIdentity();
 //var JSEnc
-var courier = new JSEncrypt();
+//var courier = new JSEncrypt();
 //console.log(encrypt);
-var public_key = courier.getPublicKey();
-var private_key = courier.getPrivateKey();
+//var private_key = courier.getPrivateKey();
 //console.log(public_key);
 //console.log(private_key);
 
-var message = 'beijing,100872';
+//var message = 'beijing,100872';
 
 //var encrypted = courier.encrypt(message);
 //console.log(encrypted);
@@ -27,10 +26,10 @@ var message = 'beijing,100872';
 //var decrypted = courier.decrypt(encrypted);
 //console.log(decrypted);
 
-var tempCourier = new JSEncrypt();
-tempCourier.setPublicKey(public_key);
-var encrypted = tempCourier.encrypt(message);
-console.log(tempCourier);
-console.log(encrypted);
-var decrypted = courier.decrypt(encrypted)
-console.log(decrypted);
+//var tempCourier = new JSEncrypt();
+//tempCourier.setPublicKey(public_key);
+//var encrypted = tempCourier.encrypt(message);
+//console.log(tempCourier);
+//console.log(encrypted);
+//var decrypted = courier.decrypt(encrypted)
+//console.log(decrypted);
